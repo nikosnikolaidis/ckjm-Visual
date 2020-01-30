911c87e5224f58c89b245e02931ddcb5437813ed
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-01-30 22:47:25 +0200

    minor Changes

--- src/ckjmvisual/Main.java
+++ src/ckjmvisual/Main.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ckjmvisual;
 
 
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Please give me access!");
+        System.out.println("Please give me access!");   
+        
+        
     }
     
 }
