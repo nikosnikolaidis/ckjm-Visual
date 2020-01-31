611cb82ec983bda701fc873ba02c8f51743adc05
nikosnikolaidis <nikos.gate4.nik@gmail.com>
2020-01-31 12:16:45 +0200

    Added metrics of ckjm as properties

--- src/ckjmvisual/JavaFile.java
+++ src/ckjmvisual/JavaFile.java
@@ -5,14 +5,55 @@ package ckjmvisual;
 public class JavaFile {
     
     private String fileName;
-    
-    public JavaFile(String fileName){
-        this.fileName=fileName;
-    }
+    private int WMC;
+    private int DIT;
+    private int NOC;
+    private int CBO;
+    private int RFC;
+    private int LCOM;
+    private int Ca;
+    private int NPM;
 
+    public JavaFile(String fileName, int WMC, int DIT, int NOC, int CBO, int RFC, int LCOM, int Ca, int NPM) {
+        this.fileName = fileName;
+        this.WMC = WMC;
+        this.DIT = DIT;
+        this.NOC = NOC;
+        this.CBO = CBO;
+        this.RFC = RFC;
+        this.LCOM = LCOM;
+        this.Ca = Ca;
+        this.NPM = NPM;
+    }
+    
+    
     
     // Getters //
     public String getFileName() {
         return fileName;
+    }
+    public int getWMC() {
+        return WMC;
+    }
+    public int getDIT() {
+        return DIT;
+    }
+    public int getNOC() {
+        return NOC;
+    }
+    public int getCBO() {
+        return CBO;
+    }
+    public int getRFC() {
+        return RFC;
+    }
+    public int getLCOM() {
+        return LCOM;
+    }
+    public int getCa() {
+        return Ca;
+    }
+    public int getNPM() {
+        return NPM;
     }
 }
