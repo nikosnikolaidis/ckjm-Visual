8bf159fba8bccef185c7792bc90d75e79f8e8394
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-01-31 13:24:11 +0200

    Features: Created dummy file reader on Main

--- src/ckjmvisual/Main.java
+++ src/ckjmvisual/Main.java
@@ -2,7 +2,7 @@
 package ckjmvisual;
 
 import java.util.ArrayList;
-
+import java.io.*;
 
 public class Main {
     
@@ -11,9 +11,17 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         HomeFrame homeFrame=new HomeFrame();
         homeFrame.setVisible(true);
+        //Read dummy file from location - replace with Project.directory
+        File file = new File("C:\\Users\\Sakis\\Desktop\\doc.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
     }
     
 }
