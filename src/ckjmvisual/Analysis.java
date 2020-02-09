d8c6d87b004fb32726aef007ec97d26421c7ba31
DESKTOP-96Q00G1\Sakis <sthanasis1994@gmail.com>
2020-02-09 22:26:28 +0200

    Created Analysis class

---
+++ src/ckjmvisual/Analysis.java
@@ -0,0 +1,24 @@
+
+package ckjmvisual;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Analysis {
+    private Date date;
+    private ArrayList<JavaFile> javaFiles;
+    
+    public Analysis(ArrayList javaFiles){
+        
+        this.date = new Date();
+        this.javaFiles = javaFiles;
+        
+    }
+    
+    public Date getDate(){
+        return this.date;
+    }
+    
+    public ArrayList<JavaFile> getJavaFiles(){
+        return this.javaFiles;
+    }
+}
--- src/ckjmvisual/Main.java
+++ src/ckjmvisual/Main.java
@@ -12,8 +12,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args){
+        
         HomeFrame homeFrame=new HomeFrame();
         homeFrame.setVisible(true);
+        
     }
     
 
